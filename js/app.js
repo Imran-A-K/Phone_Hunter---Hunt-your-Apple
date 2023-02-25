f49d5e4abcb8294f9cfb2fffa023ff903ff8d536
Imran-A-K <imran.k.professional@gmail.com>
2023-02-25 23:36:06 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const loadPhones = async() =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const response = await fetch(url)
+    const data = await response.json()
+    displayPhones(data.data)
+}
+const displayPhones = phones =>{
+
+}
+loadPhones()
