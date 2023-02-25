c232bf8c554b67bf9d5a6d63fbca04a2160df11d
Imran-A-K <imran.k.professional@gmail.com>
2023-02-26 00:10:47 +0600

    Displaying data within cards and addind search functionality

--- js/app.js
+++ js/app.js
@@ -5,6 +5,20 @@ const loadPhones = async() =>{
     displayPhones(data.data)
 }
 const displayPhones = phones =>{
-
+    const phonesContainer = document.getElementById('phone-container')
+    phones.forEach(phone =>{
+    //   const phoneDiv  = document.createElement('div')
+    phonesContainer.innerHTML += `
+    <div class="col">
+                      <div class="card p-4">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">${phone.name}</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                      </div>
+                    </div>
+    `
+    })
 }
 loadPhones()
